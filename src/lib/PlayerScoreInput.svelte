9833628f9052a01107414328c703a9f0a43fbb9e
Gérald Gounot <gerald@gounot.eu>
2023-04-19 00:33:31 +0200

    Add PlayerScoreInput

---
+++ src/lib/PlayerScoreInput.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { playerColorBg } from "../constants";
+
+  export let playerColor: PlayerColor;
+  export let playerName: string;
+  export let stackCards: number = 0;
+  export let centerCards: number = 0;
+</script>
+
+<div class={`card-compact ${playerColorBg[playerColor]}`}>
+  <div class="card-body">
+    <h2 class="card-title">
+      {playerName}
+    </h2>
+    <table class="text-center">
+      <thead
+        ><tr
+          ><th /><th scope="col">Cards</th><th scope="col">Points</th><th
+            scope="col">Total</th
+          ></tr
+        ></thead
+      >
+      <tbody
+        ><tr
+          ><th scope="row">Stack</th><td
+            ><input
+              type="number"
+              min="0"
+              bind:value={stackCards}
+              name="stack-cards"
+              class="w-12 text-center"
+            /></td
+          ><td>{stackCards * -2}</td><td rowspan="2" class="text-4xl"
+            >{stackCards * -2 + centerCards}</td
+          ></tr
+        ><tr
+          ><th scope="row">Table</th><td
+            ><input
+              type="number"
+              min="0"
+              bind:value={centerCards}
+              name="center-cards"
+              class="w-12 text-center"
+            /></td
+          ><td>{centerCards}</td></tr
+        ></tbody
+      >
+    </table>
+  </div>
+</div>
