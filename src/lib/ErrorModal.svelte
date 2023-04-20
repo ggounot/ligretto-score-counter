1d6af5f55ff9a132f3e4f1141288869f594e4297
Gérald Gounot <gerald@gounot.eu>
2023-04-21 00:38:00 +0200

    Add ErrorModal component

---
+++ src/lib/ErrorModal.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let errors: string[];
+  export let open: boolean;
+</script>
+
+<div class="modal" class:modal-open={open}>
+  <div class="modal-box">
+    <h3 class="text-lg font-bold">
+      {errors.length > 1 ? "Errors:" : "Error:"}
+    </h3>
+    <ul class="list-inside list-disc">
+      {#each errors as error}
+        <li>{error}</li>
+      {/each}
+    </ul>
+    <div class="modal-action">
+      <button class="btn" on:click={() => (open = false)}>OK</button>
+    </div>
+  </div>
+</div>
