4500d088b0d7286aee8e312cdaeb6bd5b372b24a
Gérald Gounot <gerald@gounot.eu>
2023-04-20 23:54:48 +0200

    types.ts: add new types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,27 @@
+type PlayerId = number;
+
 type PlayerColor = "orange" | "blue" | "green" | "yellow";
+
+type Player = { id: PlayerId; name: string; color: PlayerColor };
+
+type RoundPlayerScore = {
+  playerId: PlayerId;
+  playerstackCards: number;
+  tableCards: number;
+  stackPoints: number;
+  tablePoints: number;
+  score: number;
+};
+
+type Round = RoundPlayerScore[];
+
+type GamePlayerScore = { playerId: PlayerId; score: number };
+
+type GameScore = GamePlayerScore[];
+
+type Game = {
+  date: Date;
+  players: Player[];
+  rounds: Round[];
+  score: GameScore;
+};
