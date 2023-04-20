bc3d5817fd2a199993307a6878a999fdd4f2e908
Gérald Gounot <gerald@gounot.eu>
2023-04-20 18:37:58 +0200

    App: add header and router

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,41 @@
 <script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+
+  import { Router, Link, Route } from "svelte-routing";
+  import { globalHistory } from "svelte-routing/src/history";
+
+  import GameList from "./lib/GameList.svelte";
+
+  export let url = "";
+
+  let pathname = window.location.pathname;
+  let unsub;
+
+  onMount(() => {
+    // Listen to location change and update the pathname
+    unsub = globalHistory.listen(({ location }) => {
+      pathname = location.pathname;
+    });
+  });
+
+  // If the current pathname is not root, display the back button
+  $: backButton = pathname !== "/";
+
+  onDestroy(() => {
+    unsub();
+  });
 </script>
 
-<main />
+<Router {url}>
+  <header class="navbar justify-between">
+    <img src="/vite.svg" alt="Ligretto Score Counter" />
+    <nav>
+      {#if backButton}
+        <Link class="btn-ghost btn-sm btn" to="/">Back to Game List</Link>
+      {/if}
+    </nav>
+  </header>
+  <main>
+    <Route path="/"><GameList /></Route>
+  </main>
+</Router>
