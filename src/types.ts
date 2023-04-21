928cdb080839eefd1da7f56211518ba76f41f73a
Gérald Gounot <gerald@gounot.eu>
2023-04-21 11:09:03 +0200

    types: refactor RoundPlayerScore and Round

--- src/types.ts
+++ src/types.ts
@@ -5,15 +5,14 @@ type PlayerColor = "orange" | "blue" | "green" | "yellow";
 type Player = { id: PlayerId; name: string; color: PlayerColor };
 
 type RoundPlayerScore = {
-  playerId: PlayerId;
-  playerstackCards: number;
+  stackCards: number;
   tableCards: number;
   stackPoints: number;
   tablePoints: number;
   score: number;
 };
 
-type Round = RoundPlayerScore[];
+type Round = Record<PlayerId, RoundPlayerScore>;
 
 type GamePlayerScore = { playerId: PlayerId; score: number };
 
