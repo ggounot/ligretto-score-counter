0e3e162cf56716a00af63a66887b8399eb6d9014
Gérald Gounot <gerald@gounot.eu>
2023-05-16 11:46:37 +0200

    tailwind.config.js: add index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{svelte,js,ts}"],
+  content: ["./index.html", "./src/**/*.{svelte,js,ts}"],
   theme: {
     extend: {},
   },
