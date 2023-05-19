1d3f13629b23fdfded8d70d3ccbe7cf2f916d8a2
Gérald Gounot <gerald@gounot.eu>
2023-05-19 11:18:58 +0200

    Configure Vite PWA and move icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,75 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeManifestIcons: false,
+      includeAssets: [
+        "icons/favicon-16x16.png",
+        "icons/favicon-32x32.png",
+        "logo.webp",
+      ],
+      manifest: {
+        name: "Ligretto Score Counter",
+        short_name: "Ligretto Score",
+        description: "Easily calculate the score of your Ligretto games!",
+        categories: ["games", "utilities"],
+        icons: [
+          {
+            src: "icons/192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/192-maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icons/384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icons/384-maskable.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/512-maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icons/1024.png",
+            sizes: "1024x1024",
+            type: "image/png",
+          },
+          {
+            src: "icons/1024-maskable.png",
+            sizes: "1024x1024",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#570df8",
+        background_color: "#FFFFFF",
+      },
+    }),
+  ],
 });
