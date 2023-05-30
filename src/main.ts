990a169c7810384273b5c5ae9d0dc8dd9b92638b
Gérald Gounot <gerald@gounot.eu>
2023-05-30 18:18:21 +0200

    Configure Sentry

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import "./app.css";
 import App from "./App.svelte";
 
+import * as Sentry from "@sentry/svelte";
+
+// Initialize the Sentry SDK here
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [new Sentry.BrowserTracing({}), new Sentry.Replay()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.1,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 const app = new App({
   target: document.getElementById("app"),
 });
