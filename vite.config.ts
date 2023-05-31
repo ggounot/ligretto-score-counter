2e233aada1a1b2b1715ba431b1c2530c2d1cef97
Gérald Gounot <gerald@gounot.eu>
2023-05-31 10:48:04 +0200

    Configure Sentry plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { VitePWA } from "vite-plugin-pwa";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+
+const env = loadEnv("mock", process.cwd(), "");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     svelte(),
     VitePWA({
@@ -115,5 +121,16 @@ export default defineConfig({
         background_color: "#FFFFFF",
       },
     }),
+    sentryVitePlugin({
+      org: env.SENTRY_ORG,
+      project: env.VITE_SENTRY_PROJECT,
+      authToken: env.SENTRY_AUTH_TOKEN,
+      release: {
+        name: `${env.VITE_SENTRY_PROJECT}@${env.npm_package_version}`,
+      },
+    }),
   ],
+  define: {
+    "import.meta.env.PACKAGE_VERSION": JSON.stringify(env.npm_package_version),
+  },
 });
